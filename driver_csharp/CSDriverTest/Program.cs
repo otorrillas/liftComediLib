b956e6da5e4530b8e496aedc35a2180f3281218e
DanglingPointer <mikail.vasilyev@gmail.com>
2016-02-21 23:10:36 +0100

    OO wrapper for c#

--- driver_csharp/CSDriverTest/Program.cs
+++ driver_csharp/CSDriverTest/Program.cs
@@ -1,31 +1,69 @@
-// Test equivalent to the main.c file at https://github.com/TTK4145/Project/blob/master/driver/main.c
+// Test equivalent to https://github.com/TTK4145/Project/blob/master/driver/main.c with some extras
 using System;
+using System.Threading.Tasks;
 namespace Elev.Test
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            LibElev.Init();
-
-            Console.WriteLine("Press stop button to stop elevator and exit program\n");
-
-            LibElev.Set.MotorDirection(Dirn.Up);
-
-            while (true)
+            try
             {
-                // Change direction when we reach top/bottom floor
-                if (LibElev.Get.FloorSensorSignal() == C.nFloors - 1)
-                    LibElev.Set.MotorDirection(Dirn.Down);
-                else if (LibElev.Get.FloorSensorSignal() == 0)
-                    LibElev.Set.MotorDirection(Dirn.Up);
+                ICar car = Elevator.GetElevator();
+                IPanel panl = Elevator.GetPanel();
 
-                // Stop elevator and exit program if the stop button is pressed
-                if (LibElev.Get.StopSignal() != 0)
+                // Extra: updating floor lights as the elevator passes by
+                Task.Run(() =>
                 {
-                    LibElev.Set.MotorDirection(Dirn.Stop);
-                    return;
+                    while (true)
+                    {
+                        int floor = car.FloorSensorSignal;
+                        if (floor != -1)
+                            panl.FloorIndicator = floor;
+                    }
+                });
+
+                // Extra: turning on button lights when pressed
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        for(int floor=0; floor < Elevator.FloorCount; ++floor)
+                            for(int button = 0; button < Elevator.ButtonCount; ++button)
+                                if (panl.ButtonSignal[(Button)button, floor] == true)
+                                    panl.ButtonLamp[(Button)button, floor] = true;
+                    }
+                });
+
+                Console.WriteLine("Press stop button to stop elevator and exit program\n");
+
+                car.MotorDirection = Direction.Up;
+
+                while (true)
+                {
+                    // Change direction when we reach top/bottom floor
+                    if (car.FloorSensorSignal == Elevator.FloorCount - 1)
+                        car.MotorDirection = Direction.Down;
+                    else if (car.FloorSensorSignal == 0)
+                        car.MotorDirection = Direction.Up;
+
+                    // Stop elevator and exit program if the stop button is pressed
+                    if (car.StopSignal == true)
+                    {
+                        car.MotorDirection = Direction.Stop;
+                        return;
+                    }
                 }
+            }
+            catch (DllNotFoundException)
+            {
+                Console.WriteLine("Put libelev.so in the same folder as your exe-file!\n");
+                Console.Read(); // Prevent console from closing
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.Read(); // Prevent console from closing
             }
         }
     }
